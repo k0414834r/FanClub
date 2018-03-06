29e5332336eb439dfe8d953902d10a9878f5e57c
Boian Etropolski <boetropolski@gmail.com>
2018-03-05 23:26:38 -0600

    add LoginForm + route, secret key, & current_user authentication

--- run.py
+++ run.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import LoginForm
+from flask_login import current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'RNG'
 
 @app.route("/")
-
-@app.route('/index')
+@app.route('/home')
 def index():
-
     user1 = {'username': 'Boian'}
-
-    return render_template('index.html', user=user1)
+    return render_template('home.html', user=user1)
 
 @app.route('/clubplans')
 def clubplans():
-
     message = "Hello, Subscriber!"
+    return render_template('clubplans.html', message=message)
 
-    return render_template('clubplans.html', message = message)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 app.run()
