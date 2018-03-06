29d3b621cacd95861a6fa9078b2710e0f03e16ed
Boian Etropolski <boetropolski@gmail.com>
2018-03-05 22:15:38 -0600

    add index route + login/logout template

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return 'Hello, World!'
+
+@app.route('/index')
+def index():
+
+    user1 = {'username': 'Boian'}
+
+    return render_template('index.html', user=user1)
+
 
 app.run()
